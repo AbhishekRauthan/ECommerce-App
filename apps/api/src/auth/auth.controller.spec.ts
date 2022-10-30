011ac8723dfef7b0219b74f75d90e18d60a97b79
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-10-30 15:41:05 +0530

    The following was done:
    	- created and implemented registerUser function along with its tests.
    	- function would throw conflict error if user already exists or bad request error if unable to create user.

--- apps/api/src/auth/auth.controller.spec.ts
+++ apps/api/src/auth/auth.controller.spec.ts
@@ -1,18 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserRegisterDTO } from '../dto/user.dto';
+import { TypeormService } from '../shared/typeorm/typeorm.service';
+import { Roles } from '../types/index.types';
 import { AuthController } from './auth.controller';
+
+type MockType<T> = {
+  [P in keyof T]?: jest.Mock<Record<string, unknown>>;
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
-
-  beforeEach(async () => {
+  const typeormServiceMock: MockType<TypeormService> = {
+    findUserByEmail: jest.fn(),
+  };
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: TypeormService,
+          useValue: typeormServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
   });
+  describe('User Register Test', () => {
+    it('should throw conflict error', async () => {
+      const user: UserRegisterDTO = {
+        email: 'test@example.com',
+        name: 'test user',
+        password: 'test_password',
+        role: Roles.User,
+      };
+      typeormServiceMock.findUserByEmail = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(user));
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+      try {
+        await controller.registerUser(user);
+      } catch (error) {
+        expect(error.response.message).toBe(
+          "User with email: 'test@example.com' already exists!"
+        );
+      }
+    });
+
+    it('should throw bad request', async () => {
+      const user: UserRegisterDTO = {
+        email: 'test@example.com',
+        name: 'test user',
+        password: 'test_password',
+        role: Roles.User,
+      };
+      typeormServiceMock.findUserByEmail = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(null));
+
+      typeormServiceMock.createUser = jest
+        .fn()
+        .mockImplementation(() => Promise.reject(null));
+      try {
+        await controller.registerUser(user);
+      } catch (error) {
+        expect(error.response.message).toBe('Unable to create user');
+      }
+    });
   });
 });
--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, NotImplementedException, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  NotImplementedException,
+  Post,
+} from '@nestjs/common';
+import { UserRegisterDTO } from '../dto/user.dto';
+import { TypeormService } from '../shared/typeorm/typeorm.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private typeormService: TypeormService) {}
+
   @Post('/login')
   loginUser() {
     throw new NotImplementedException('Not Worked not it');
   }
 
   @Post('/register')
-  registerUser() {
-    throw new NotImplementedException('Not Worked not it');
+  async registerUser(@Body() userInfo: UserRegisterDTO) {
+    const { email } = userInfo;
+    const userExists = await this.typeormService.findUserByEmail(email);
+    if (userExists) {
+      throw new ConflictException(
+        `User with email: '${email}' already exists!`
+      );
+    }
+    return await this.typeormService.createUser(userInfo).catch(() => {
+      throw new BadRequestException('Unable to create user');
+    });
   }
 
   @Post('/admin/login')
