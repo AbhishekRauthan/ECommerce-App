433b0b11599ab1daeb1802c727c6a1d82b6cf461
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-11-20 21:17:10 +0530

    The following was done:
    	- implemented jwt stratergy that returns object with name, email, id, role.
    	- implemented jwt auth gaurd which on err ar user not found throw Unauthorized Exception.
    	- imported jwt module with .env configuration service to get jwt secret

---
+++ apps/api/src/shared/jwt/jwt.stratergy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from '../model/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    configService: ConfigService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { id: number }) {
+    const user = await this.usersRepository.findOneBy({ id: payload.id });
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+  }
+}
--- apps/api/src/shared/shared.module.ts
+++ apps/api/src/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { Global, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfigService } from './typeorm/typeormConfig.service';
 import { TypeormService } from './typeorm/typeorm.service';
@@ -7,6 +7,8 @@ import { Product } from './model/product.entity';
 import { Item } from './model/item.entity';
 import { Order } from './model/order.entity';
 import { User } from './model/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.stratergy';
 
 @Global()
 @Module({
@@ -19,8 +21,17 @@ import { User } from './model/user.entity';
       useClass: TypeormConfigService,
     }),
     TypeOrmModule.forFeature([Product, Item, Order, User]),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '30d' },
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
-  providers: [TypeormService],
-  exports: [TypeormService, TypeOrmModule],
+  providers: [TypeormService, JwtStrategy],
+  exports: [TypeormService, TypeOrmModule, JwtStrategy, JwtModule],
 })
 export class SharedModule {}
