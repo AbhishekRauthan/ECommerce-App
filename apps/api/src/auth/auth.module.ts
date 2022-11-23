36820fc5fb3e9574ca88e00983ba5e68c2027c02
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-11-23 22:10:40 +0530

    removed jwt stratergy from auth module providers

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { JwtStrategy } from '../shared/jwt/jwt.stratergy';
 import { TypeormService } from '../shared/typeorm/typeorm.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [TypeormService, JwtStrategy],
+  providers: [TypeormService],
   controllers: [AuthController],
 })
 export class AuthModule {}
